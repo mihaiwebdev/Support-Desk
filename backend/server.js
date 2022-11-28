e529f5aef4c74a0da87c526e2c4b3cf6e7a6ef44
mihaiwebdev <1mihaisava1@gmail.com>
2022-11-28 19:15:35 +0200

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 //  import express
+const path = require('path')
 const express =require('express')
-
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
@@ -18,15 +18,23 @@ app.use(express.json())
 // accept url encoded form
 app.use(express.urlencoded({extended: false}))
 
-// create route
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Welcome to the Support Desk API'})
-})
-
 // routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
-app.use(errorHandler)
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+    // Set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
 
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+
+} else {
+
+    app.get('/', (req, res) => {
+        res.status(200).json({message: 'Welcome to the Support Desk API'})
+    })
+}
+
+app.use(errorHandler)
 app.listen(PORT, () => console.log(`listening to port ${PORT}`))
